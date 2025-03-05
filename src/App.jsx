670283b73b7cf8f5a6122aa5b761681aae685496
1997MohitAgarwal <152138497+1997MohitAgarwal@users.noreply.github.com>
2025-03-05 16:14:28 +0530

    File name was going outside for mobile in some cases fixed

--- src/App.jsx
+++ src/App.jsx
@@ -243,7 +243,7 @@ const apiKey = `${part1}${part2}${part3}`;
             <div className="mb-12">
               <label className="w-full flex flex-col items-center justify-center p-12 border-2 border-dashed border-rose-400 rounded-2xl bg-gray-50 hover:bg-gray-100 transition-all duration-300 cursor-pointer">
                 <UploadCloud className="w-20 h-20 text-rose-300 mb-6" />
-                <span className="text-xl lg:text-2xl text-center font-semibold text-gray-800">
+                <span className="text-lg lg:text-2xl text-center font-semibold break-words w-full text-gray-800">
                   {file ? file.name : "Upload Your Resume Now"}
                 </span>
                 <input
